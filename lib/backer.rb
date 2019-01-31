4d9660eff6c29283f5f1b37dfb09f3b1c08afb68
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 00:08:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer 
   attr_accessor :name 
+  @backed_projects = []
   
   def initialize(name)
     @name = name
   end
   
   def back_project(project)
-    backed_projects = []
-    backed_projects << project 
+    @backed_projects << project 
   end 
   
 end 
